ad99656b0f2c38f33533b7ff149c5cc9ffcbe3a2
Elihu Garrett <pmaekb@hotmail.com>
2017-04-17 16:08:34 -0500

    Create blue_drops.lua

---
+++ blue_drops.lua
@@ -0,0 +1,59 @@
+
+local app = {}
+
+function app.liveconf(t)
+  t.live = true
+  t.use_pcall = true
+  t.autoreload.enable = true
+  t.autoreload.interval = 1.0
+  t.reloadkey = "f5"
+  t.gc_before_reload = false
+  t.error_file = nil  -- "error.txt"
+end
+
+function app.load()
+  value1 = 0
+  time = 0
+  app.reload()
+end
+
+function app.reload()
+    myShader = love.graphics.newShader[[
+      extern number iGlobalTime;
+      extern number val1;
+
+      vec4 effect( vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords ){
+        vec2 sp = screen_coords.xy / love_ScreenSize.xy; - 1.0;
+        vec2 p = sp * 10.0 - vec2(10.0);
+        vec2 i = p;
+        float c = 1.0;
+        float inten = 0.01;
+        float t = (iGlobalTime * 0.9) * (1.0 - (1.2 / float(0+1)));
+        i = p / vec2(cos(t - i.x) + sin(t + i.y), sin(t - i.y) + cos(t + i.x));
+        c += 1.0/length(vec2(p.x / (sin(i.x + t) / inten), p.y / (tan(i.y + t) / inten)));
+        c /= 1.0 + val1;
+        c = 1.5 - sqrt(c);
+        return vec4(vec3(c*c*c+c), 999.0) + vec4(0.0, 1.0, 1.0, 1);
+      }
+   ]]
+end
+
+function app.update(dt)
+  time = time + dt;
+	myShader:send("iGlobalTime", time)
+  myShader:send("val1", value1)
+  if love.keyboard.isDown"q" then value1 = 1 
+    else value1 = 0
+  end
+end
+
+function app.draw()
+  local width = love.graphics.getWidth()
+  local height = love.graphics.getHeight()
+  love.graphics.setShader(myShader)
+	love.graphics.rectangle("fill", 0, 0, width, height)
+  love.graphics.setShader()
+  love.timer.sleep(.001)
+end
+
+return app
